2db708db68d3ef33da2fd1cf81ce019f024889c4
Jafesu <Jafesu@users.noreply.github.com>
2022-07-10 23:19:26 -0500

    Upload stuff

---
+++ 67impala/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'bodacious'
+game 'gta5'
+
+ -- Head over to https://docs.fivem.net/game-references/data-files/ for the data files
+ -- REMEMBER TO REMOVE THE FILE FROM files and data_file IF NOT IN RESOURCE!!
+ 
+files {
+    'data/*.meta'
+}
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
